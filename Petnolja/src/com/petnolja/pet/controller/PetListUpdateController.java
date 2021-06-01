a0f6208329ada559afce3d65857e4f276a64557c
nald <daita0225@gmail.com>
2021-06-01 21:00:21 +0900

    Update PetListUpdateController.java

--- Petnolja/src/com/petnolja/pet/controller/PetListUpdateController.java
+++ Petnolja/src/com/petnolja/pet/controller/PetListUpdateController.java
@@ -7,8 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 import com.oreilly.servlet.MultipartRequest;
